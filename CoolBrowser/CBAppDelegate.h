9dedf81adbd4a6c312ae8341cd7f07d118bac960
Brent Anderson <me@brentjanderson.com>
2013-10-30 17:19:44 -0600

    Initial Commit

---
+++ CoolBrowser/CBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CBAppDelegate.h
+//  CoolBrowser
+//
+//  Created by Brent Anderson on 10/30/13.
+//  Copyright (c) 2013 BYU AIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoolBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoolBrowser
+//
+//  Created by Brent Anderson on 10/30/13.
+//  Copyright (c) 2013 BYU AIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CBAppDelegate class]));
+    }
+}
